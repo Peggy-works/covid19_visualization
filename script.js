b890f1e551e0bf689a3528e3803c15e20cacbdf9
Peggys-jpg <samoraangel26@gmail.com>
2021-06-17 18:57:42 -0700

    Made a few changes to the bar, still not satisfied, pushing changes but will continue fixing.

--- script.js
+++ script.js
@@ -1,73 +1,58 @@
-$(document).ready(function(){ 
-    let message; 
-    var margin = {top: 20, right: 30, bottom: 40, left: 90};
-    var width = 760 - margin.left - margin.right; //460, 400
-    var height = 600 - margin.top - margin.bottom;
-  
-    var svg = d3.select("#total_deaths")
-      .append('svg')
-      .attr('width', width + margin.left + margin.right)
-      .attr('height', height + margin.top + margin.bottom)
-      .append('g')
-      .attr('transform', "translate(" + margin.left + ", " + margin.top + ")");
-    $(window).scroll(function(event){ 
-      //message = 'oh my lord we are fucking getting scrolled on!'; 
-      var $window = $(window);
-      var $body = $('body');
-      var $head = $('.head-container');
-      var height = Math.round($window.height()/2);
-      //console.log(height);
-      //console.log($window.scrollTop() + ', ' + $window.height(), + ", " + ($window.height()/3));
-      if ($window.scrollTop() > height){
-        $body.addClass('color-brown');
-      } else if ($window.scrollTop() < height) {
-        $body.removeClass('color-brown');
-      }
-      //console.log(message);
-    });
-    console.log("?");
-    var csv_file = 'NYT_covid19_deaths.csv'; 
-    d3.csv(csv_file).then(function(data){
-      
-      /*for (var i = 0; i < data.length; i++){
-        console.log(data[i].state_name + ", " + data[i].deaths);
-      }*/
-      dataset = data.sort(function(a, b){ 
-        //console.log(typeof(a.deaths));
-        return d3.descending(parseInt(a.deaths), parseInt(b.deaths))
-      })
-  
-      // Add X axis
-    var x = d3.scaleLinear()
-      .domain([0, 13000])
-      .range([ 0, width]);
-    svg.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x))
-      .selectAll("text")
-        .attr("transform", "translate(-10,0)rotate(-45)")
-        .style("text-anchor", "end");
-  
-    // Y axis
-    var y = d3.scaleBand()
-      .range([ 0, height ])
-      .domain(data.map(function(d) { return d.state_name; }))
-      .padding(.1);
-    svg.append("g")
-      .call(d3.axisLeft(y))
-  
-    //Bars
-    svg.selectAll("myRect")
-      .data(data)
-      .enter()
-      .append("rect")
-      .attr("x", x(0) )
-      .attr("y", function(d) { return y(d.state_name); })
-      .attr("width", function(d) { return x(d.deaths); })
-      .attr("height", y.bandwidth() )
-      .attr("fill", "#69b3a2")
-      for (var i = 0; i < dataset.length; i++){
-        console.log(dataset[i].state_name, ', ' + dataset[i].deaths);
-      }
-    })  
+$(document).ready(function(){  
+  let message;  
+  var dates = [];
+  var margin; 
+  var svg = d3.select('svg')
+    .attr('width', parseInt($(document).height()))
+    .attr('height', 800)
+  var g = d3.select('g')
+    .attr('transform', "translate(20,30)");
+  $(window).scroll(function(event){  
+    var $window = $(window);
+    var $body = $('body');
+    var $head = $('.head-container');
+    var height = Math.round($window.height()/2); 
+    if ($window.scrollTop() > height){
+      $body.addClass('color-brown');
+    } else if ($window.scrollTop() < height) {
+      $body.removeClass('color-brown');
+    } 
   });
+  console.log("?");
+  var csv_file = 'NYT_covid19_deaths.csv'; 
+  d3.csv(csv_file).then(function(data){ 
+    dataset = data.sort(function(a, b){  
+      return d3.descending(parseInt(a.deaths), parseInt(b.deaths))
+    })  
+    console.log(dataset[0].deaths + ", " + dataset[dataset.length-1].deaths);  
+    for (var i = 0; i < dataset.length; i++){
+      dates.push(dataset[i].state_name);
+    } 
+    //console.log('printing first, ' + dates[0]); 
+    // x
+    var x = d3.scaleLinear()
+      .domain([0, dataset[0].deaths])
+      .range([0, parseInt($(document).width())]);
+
+    // y
+    var y = d3.scaleBand()
+      .domain(dates)
+      .range([0, 600]) 
+      .round(true)
+      .paddingInner(2);
+    //console.log(x)
+    console.log(y.bandwidth());
+    d3.select('.horizontal-bar-chart')
+      .selectAll('rect') 
+      .data(dataset)
+      .join('rect')
+      .classed('bar', true)
+      .attr("y", d => y(d.state_name)) //.attr("y", d => y(d.state_name))
+      .attr("x", x(0))
+      .attr('height', 10)
+      .attr('width', d => x(d.deaths))
+    /*for (var i = 0; i < dataset.length; i++){
+      console.log(dataset[i].state_name, ', ' + dataset[i].deaths);
+    }*/
+  })  
+});
