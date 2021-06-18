376f19ad987c6dc1544e779b54870c4b7014ea22
Peggys-jpg <samoraangel26@gmail.com>
2021-06-18 16:03:05 -0700

    Graph looks finished, ticks/axis/bars look good, now need to add hover to each 'rect' so I can add a tooltip that explains the total amt of deaths per state.

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 $(document).ready(function(){  
+  // Variables
   let message;  
   var dates = [];
-  var margin; 
+
+  // Margins for 'g' element.
+  var margin = {top: 40, right: 130, bottom: 40, left: 130},
+      width = parseInt($(document).width()) - margin.left - margin.right, // margin-right
+      height = 900 - margin.top - margin.bottom;
+
+  // Width of 'svg'. 
+  var svg_width = parseInt($(document).width());
+
+  //Setting dimensions for svg, appending "g". 
   var svg = d3.select('svg')
-    .attr('width', parseInt($(document).height()))
-    .attr('height', 800)
-  var g = d3.select('g')
-    .attr('transform', "translate(20,30)");
+    .attr('width', svg_width) //width
+    .attr('height', height) 
+    .append('g')
+    .attr('transform', "translate(" + margin.left + "," + margin.top + ")")
+    .attr('class', 'horizontal-bar-graph'); 
+
+
+  // Changing color scheme based on where we at in the webpage.
   $(window).scroll(function(event){  
     var $window = $(window);
     var $body = $('body');
@@ -17,8 +31,9 @@ $(document).ready(function(){
     } else if ($window.scrollTop() < height) {
       $body.removeClass('color-brown');
     } 
-  });
-  console.log("?");
+  }); 
+
+  // Opening csv file to read in data.
   var csv_file = 'NYT_covid19_deaths.csv'; 
   d3.csv(csv_file).then(function(data){ 
     dataset = data.sort(function(a, b){  
@@ -29,30 +44,61 @@ $(document).ready(function(){
       dates.push(dataset[i].state_name);
     } 
     //console.log('printing first, ' + dates[0]); 
-    // x
-    var x = d3.scaleLinear()
-      .domain([0, dataset[0].deaths])
-      .range([0, parseInt($(document).width())]);
 
-    // y
+    // X-Axis 
+    console.log(width);
+    var x = d3.scaleLinear()
+      .domain([0, dataset[0].deaths]) //dataset[0].deaths 
+      .range([0, width]);
+
+    // Adding X-Axis to svg
+    svg.append('g')
+      .attr("transform", "translate(0, 750)")
+      .attr("class", 'x-axis')
+      .call(d3.axisBottom(x).tickValues([0, 5000, 10000, 15000, 20000, 25000, 30000, 35000, 40000, 45000, 50000, 55000, 60000, dataset[0].deaths]));
+
+    // Y-Axis
     var y = d3.scaleBand()
       .domain(dates)
-      .range([0, 600]) 
-      .round(true)
-      .paddingInner(2);
-    //console.log(x)
-    console.log(y.bandwidth());
-    d3.select('.horizontal-bar-chart')
-      .selectAll('rect') 
+      .range([0, height - margin.bottom - margin.top])  
+      .padding(.5); 
+
+    // Adding Y-axis to svg
+    svg.append('g')
+      .attr('transform', 'translate(-5,1)')
+      .attr('class', 'y-axis')
+      .call(d3.axisLeft(y));
+
+    // Adding 'rect' w/ data to svg after axis.
+    svg.selectAll('.bar')
       .data(dataset)
-      .join('rect')
-      .classed('bar', true)
-      .attr("y", d => y(d.state_name)) //.attr("y", d => y(d.state_name))
+      .enter().append('rect') 
+      .attr('class', 'bar')
+      .attr("y", d => y(d.state_name))
       .attr("x", x(0))
       .attr('height', 10)
-      .attr('width', d => x(d.deaths))
-    /*for (var i = 0; i < dataset.length; i++){
-      console.log(dataset[i].state_name, ', ' + dataset[i].deaths);
-    }*/
-  })  
+      .attr('width', d => x(d.deaths)); 
+
+    // Adding appropriate state_name to each rect
+    var rect = d3.selectAll('rect');
+    rect.each(function(d){
+      this.classList.add(d.state_name.split(" ").join(""));
+    })
+    
+    // Implementing tooltip to show data if client hovers bars.
+    $('.bar').hover(function(){
+      console.log("Hovering over, " + $(this).attr('class'));
+      var class_ = $(this).attr('class');
+      class_ = class_.substr(class_.indexOf(" ") + 1);
+      console.log(class_ + ", " + class_.length);
+    });
+  })   
+  /*setTimeout(function(){
+    $('.bar').hover(function(){
+      console.log("Hovering over, " + $(this).attr('class'));
+      var class_ = $(this).attr('class');
+      class_ = class_.substr(class_.indexOf(" ") + 1);
+      console.log(class_ + ", " + class_.length);
+    });
+  }, 300);*/
 });
